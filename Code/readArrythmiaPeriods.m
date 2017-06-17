5a8b6d298bf0139ff98741f3fadc5601b9837372
Davi Kawasaki <davishinjik@gmail.com>
2017-06-17 14:51:21 -0300

    Extract period and time relevant data to a cell

---
+++ Code/readArrythmiaPeriods.m
@@ -0,0 +1,27 @@
+function arrhythmiaPeriods = readArrythmiaPeriods(type, filename) 
+
+% usage: arrhythmiaPeriods = readArrythmiaPeriods(VT, 'data/200m') 
+%
+% This function read the annotations file and extract the respective arrhythmia periods.
+%
+% arrhythmiaPeriods{i,1} equals to minutes sample
+% arrhythmiaPeriods{i,2} equals to seconds sample
+% arrhythmiaPeriods{i,3} equals to period sample
+% arrhythmiaPeriods{i,4} equals to arrhythmia type sample
+%
+
+% Last version
+% readArrythmiaPeriods.m           D. Kawasaki			16 June 2017
+% 		      Davi Kawasaki	       16 June 2017 version 1.0
+
+type = strcat('(', type);
+annotationsEcg = readAnnotations(filename);
+arrhythmiaPeriods = {};
+for i = 1:size(annotationsEcg{1})
+    if(strcmp(annotationsEcg{1,4}(i),type))
+        inst = {annotationsEcg{1,1}(i) annotationsEcg{1,2}(i) annotationsEcg{1,3}(i) annotationsEcg{1,4}(i)};
+        arrhythmiaPeriods(end+1,:) = inst;
+    end
+end
+
+end
