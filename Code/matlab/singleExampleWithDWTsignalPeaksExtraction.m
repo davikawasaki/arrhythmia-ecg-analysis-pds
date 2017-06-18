57f1d78e871ba5ad45454bc415fcba98416479bd
Davi Kawasaki <davishinjik@gmail.com>
2017-06-18 17:39:40 -0300

    Updates on extraction single example + README text start

--- Code/matlab/singleExampleWithDWTsignalPeaksExtraction.m
+++ Code/matlab/singleExampleWithDWTsignalPeaksExtraction.m
@@ -1,11 +1,11 @@
-function [tmSeg,ecgsig,Fs,sizeEcgSig,timeEcgSig,annotationsEcg,qrsExtracted,tmExtracted,ecgsigTransf,qrsPeaks,locs] = singleExampleWithDWTsignalPeaksExtraction(filename, filepath, arrhythmiaType, minutes, seconds, period, minPeakHeight, minPeakDistance) 
+function [tmSeg,ecgsig,Fs,sizeEcgSig,timeEcgSig,annotationsEcg,qrsExtracted,tmExtracted,ecgsigTransf,qrsPeaks,locs] = singleExampleWithDWTsignalPeaksExtraction(filename, filepath, arrhythmiaType, minutes, seconds, period, scale, minPeakHeight, minPeakDistance) 
 
 % Usage: function [tmSeg,ecgsig,Fs,sizeEcgSig,timeEcgSig
 %                  annotationsEcg,qrsExtracted,tmExtracted
 %                  ecgsigTransf,qrsPeaks,locs] =
 %                  singleExampleWithDWTsignalPeaksExtraction('200m', '../data/200m',
 %                                                            'VT', 0, 7.517, 2706,
-%                                                            0.5, 0.150);
+%                                                            3, 0.5, 0.150);
 %
 % This function plots and extracts QRS peaks example.
 %
@@ -14,10 +14,10 @@ function [tmSeg,ecgsig,Fs,sizeEcgSig,timeEcgSig,annotationsEcg,qrsExtracted,tmEx
 % singleExampleWithDWTsignalPeaksExtraction.m           D. Kawasaki			18 June 2017
 % 		      Davi Kawasaki	       18 June 2017 version 1.0
 
-[tmSeg,ecgsig,Fs,sizeEcgSig,timeEcgSig] = loadEcgSignal('../data/100m');
-annotationsEcg = readAnnotations('../data/100m');
-[qrsExtracted, tmExtracted] = plotExtractSingleQRS(0, 7.517, 2706, sizeEcgSig, timeEcgSig, ecgsig, tmSeg, '200m', 'VT');
-ecgsigTransf = dwtSignal(qrsExtracted, 3);
-[qrsPeaks,locs] = plotDWTsignalPeaks(ecgsigTransf, tmExtracted, 0.5, 0.150);
+[tmSeg,ecgsig,Fs,sizeEcgSig,timeEcgSig] = loadEcgSignal(filepath);
+annotationsEcg = readAnnotations(filepath);
+[qrsExtracted, tmExtracted] = plotExtractSingleQRS(minute, seconds, period, sizeEcgSig, timeEcgSig, ecgsig, tmSeg, filename, arrhythmiaType);
+ecgsigTransf = dwtSignal(qrsExtracted, scale);
+[qrsPeaks,locs] = plotDWTsignalPeaks(ecgsigTransf, tmExtracted, minPeakHeight, minPeakDistance);
 
 end
